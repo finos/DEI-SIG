5a5510400860a3e8bcb689cdc0508e770708f9cf
Peter Smulovics <psmulovics@users.noreply.github.com>
2024-12-20 13:20:05 -0500

    dt

--- website/src/components/event.js
+++ website/src/components/event.js
@@ -8,13 +8,13 @@ export default function Event({title, location, time, link}) {
     var defined = function(property){
         return typeof property !== 'undefined';
     };
-
+    var dt = new Date(time).toLocaleString();
     return (
         <div className={classnames('text--center col col--4 padding', styles.feature)}>
             <div><i class="fa-solid fa-calendar-day fa-lg"></i>{title}</div>
             <div><i class="fa-solid fa-location-dot fa-lg"></i> {location}</div>
-            <div><i class="fa-solid fa-user-clock fa-lg"></i> {time}</div>
-            <div><i class="fa-solid fa-user-plus fa-lg"></i> {link}</div>
+            <div><i class="fa-solid fa-user-clock fa-lg"></i> {dt}</div>
+            <div><i class="fa-solid fa-user-plus fa-lg"></i> <a href={link}>Sign Up!</a></div>
         </div>
     );
 }
--- website/src/pages/events.js
+++ website/src/pages/events.js
@@ -15,8 +15,10 @@ function EventsHome() {
             title={`${siteConfig.title}`}
             description={`${siteConfig.tagline}`}>
             <header>
-                <h1>Global and Local Calendar</h1>
-                <p>Stay connected with the pulse of Diversity, Equity, and Inclusion efforts within the FINOS community. Our calendar highlights a range of events, from global conferences and webinars to local meetups and workshops. These events are designed to foster collaboration, share best practices, and celebrate the diverse voices that make our community thrive. Whether you're looking to network, learn, or lead, there's an opportunity waiting for you. Explore the calendar and join us in driving meaningful change across the open source and financial technology landscapes.</p>
+                <div className="container">
+                    <h1>Global and Local Calendar</h1>
+                    <p>Stay connected with the pulse of Diversity, Equity, and Inclusion efforts within the FINOS community. Our calendar highlights a range of events, from global conferences and webinars to local meetups and workshops. These events are designed to foster collaboration, share best practices, and celebrate the diverse voices that make our community thrive. Whether you're looking to network, learn, or lead, there's an opportunity waiting for you. Explore the calendar and join us in driving meaningful change across the open source and financial technology landscapes.</p>
+                </div>
             </header>
             <main>
                 {events && events.length && (
