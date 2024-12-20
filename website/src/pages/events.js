62924cc5255ef815ba0ee8243aff2713931088ba
Peter Smulovics <psmulovics@users.noreply.github.com>
2024-12-20 11:51:32 -0500

    starting events

---
+++ website/src/pages/events.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import classnames from 'classnames';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './styles.module.css';
+
+function Events() {
+
+    return (
+        <Layout
+        title={`${siteConfig.title}`}
+        description={`${siteConfig.tagline}`}>
+            <main>
+
+            </main>
+        </Layout>
+    )
+}
+
+export default Events;
