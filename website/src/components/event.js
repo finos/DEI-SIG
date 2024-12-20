2055c6a447c8c8db1e57f3360576507fc6b869bc
Peter Smulovics <psmulovics@users.noreply.github.com>
2024-12-20 13:49:18 -0500

    Link

--- website/src/components/event.js
+++ website/src/components/event.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classnames from 'classnames';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from '../pages/styles.module.css';
+import Link from '@docusaurus/Link';
 
 export default function Event({ title, location, time, link }) {
 
-    // <div><i class="fa-solid fa-user-plus fa-lg"></i> <Link to={{ pathname: link }}
-    // target="_blank">Sign Up!</Link></div>
+
     var defined = function (property) {
         return typeof property !== 'undefined';
     };
@@ -16,7 +16,8 @@ export default function Event({ title, location, time, link }) {
             <div><i class="fa-solid fa-calendar-day fa-lg"></i> {title}</div>
             <div><i class="fa-solid fa-location-dot fa-lg"></i> {location}</div>
             <div><i class="fa-solid fa-user-clock fa-lg"></i> {new Date(time).toLocaleString()}</div>
-
+            <div><i class="fa-solid fa-user-plus fa-lg"></i> <Link to={{ pathname: link }}
+    target="_blank">Sign Up!</Link></div>
         </div>
     );
 }
