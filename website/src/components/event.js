3ef341de774e308d3dee8416bcc910bc7253d861
Peter Smulovics <psmulovics@users.noreply.github.com>
2024-12-20 13:46:21 -0500

    grrr

--- website/src/components/event.js
+++ website/src/components/event.js
@@ -5,17 +5,18 @@ import styles from '../pages/styles.module.css';
 
 export default function Event({ title, location, time, link }) {
 
+    // <div><i class="fa-solid fa-user-plus fa-lg"></i> <Link to={{ pathname: link }}
+    // target="_blank">Sign Up!</Link></div>
     var defined = function (property) {
         return typeof property !== 'undefined';
     };
-    console.log(time);
+
     return (
         <div className={classnames('text--center col col--4 padding', styles.feature)}>
             <div><i class="fa-solid fa-calendar-day fa-lg"></i> {title}</div>
             <div><i class="fa-solid fa-location-dot fa-lg"></i> {location}</div>
             <div><i class="fa-solid fa-user-clock fa-lg"></i> {new Date(time).toLocaleString()}</div>
-            <div><i class="fa-solid fa-user-plus fa-lg"></i> <Link to={{ pathname: link }}
-                target="_blank">Sign Up!</Link></div>
+
         </div>
     );
 }
